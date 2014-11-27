311bc01b9d434c93d2405927cd507936e6c7c93a
Adam Lukaszczyk <dobiatowski@gmail.com>
2014-11-27 11:39:56 +0100

    Init of filters

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -1,23 +1,29 @@
 'use strict';
 
 angular.module('internalLibraryApp')
-  .controller('NavbarCtrl', function ($scope, $location, Auth) {
-    $scope.menu = [{
-      'title': 'Home',
-      'link': '/'
-    }];
+    .controller('NavbarCtrl', function ($scope, $location, Auth) {
+        $scope.menu = [
+            {
+                'title': 'Home',
+                'link': '/'
+            },
+            {
+                'title': 'Books',
+                'link': '/book/list/'
+            }
+        ];
 
-    $scope.isCollapsed = true;
-    $scope.isLoggedIn = Auth.isLoggedIn;
-    $scope.isAdmin = Auth.isAdmin;
-    $scope.getCurrentUser = Auth.getCurrentUser;
+        $scope.isCollapsed = true;
+        $scope.isLoggedIn = Auth.isLoggedIn;
+        $scope.isAdmin = Auth.isAdmin;
+        $scope.getCurrentUser = Auth.getCurrentUser;
 
-    $scope.logout = function() {
-      Auth.logout();
-      $location.path('/login');
-    };
+        $scope.logout = function () {
+            Auth.logout();
+            $location.path('/login');
+        };
 
-    $scope.isActive = function(route) {
-      return route === $location.path();
-    };
-  });
+        $scope.isActive = function (route) {
+            return route === $location.path();
+        };
+    });
