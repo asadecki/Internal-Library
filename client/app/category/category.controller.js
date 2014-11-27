4f94c01b24e1e76e6588dace4f0db1c495d1784c
Aleksander Sadecki <aleksander.sadecki@schibsted.pl>
2014-11-27 14:14:26 +0100

    Category name added before word Book

--- client/app/category/category.controller.js
+++ client/app/category/category.controller.js
@@ -3,7 +3,7 @@
 angular.module('internalLibraryApp')
     .controller('CategoryCtrl', function ($scope, $http, $routeParams) {
         $scope.books = [];
-
+        $scope.categoryName = $routeParams.name;
         $http.get('/api/categories/books/' + $routeParams.name).success(function (books) {
             console.log("xxxxx");
             $scope.books = books;
