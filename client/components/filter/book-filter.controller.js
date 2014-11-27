2d85143fed648ee844850ee415516ba2040bfbb7
Adam Lukaszczyk <dobiatowski@gmail.com>
2014-11-27 12:34:09 +0100

    pub/sub

--- client/components/filter/book-filter.controller.js
+++ client/components/filter/book-filter.controller.js
@@ -1,49 +1,66 @@
 'use strict';
 
-angular.module('internalLibraryApp').controller('ButtonsCtrl', function ($scope) {
+angular.module('internalLibraryApp').controller('ButtonsCtrl', function ($scope, $rootScope) {
 
-    var reset = function () {
-        for (var filter in $scope.filtersModel) {
-            $scope.filtersModel[filter].value = false;
-        }
-    };
+  var actions = {
+    buttonsOff: function() {
+      for (var filter in $scope.filtersModel) {
+        $scope.filtersModel[filter].value = false;
+      }
+    },
+    buttonAllOff: function() {
+      $scope.filtersModel.all.value = false;
+      $rootScope.$broadcast('x12', 6);
+    }
+  };
 
-    $scope.doAction = function (key) {
-        $scope.filtersModel[key].action && $scope.filtersModel[key].action();
-    };
+  $rootScope.$on('x12', function(ev, arg) {
+  console.log(arg);
 
-    $scope.filtersModel = {
-        all: {
-            title: "All",
-            value: true,
-            action: [ reset ]
-        },
-        papers: {
-            title: "Papers",
-            value: false,
-            action: [ offAll]
+  });
 
-        },
-        pdf: {
-            title: "PDF",
-            value: false
-        },
-        ebook: {
-            title: "E-book",
-            value: false
-        },
-        available: {
-            title: "Available",
-            value: false
-        },
-        unavailable: {
-            title: "Unavailable",
-            value: false
-        },
-        new: {
-            title: "New",
-            value: false
-        }
-    };
+  $scope.doAction = function(actionsArray) {
+    actionsArray && _.forEach(actionsArray, function(action) {
+      action();
+    });
+  };
+
+  $scope.filtersModel = {
+    all: {
+      title: "All",
+      value: true,
+      action: [ actions.buttonsOff ]
+    },
+    papers: {
+      title: "Papers",
+      value: false,
+      action: [ actions.buttonAllOff ]
+    },
+    pdf: {
+      title: "PDF",
+      value: false,
+      action: [ actions.buttonAllOff ]
+    },
+    ebook: {
+      title: "E-book",
+      value: false,
+      action: [ actions.buttonAllOff ]
+    },
+    available:  {
+      title: "Available",
+      value: false,
+      action: [ actions.buttonAllOff ]
+    },
+    unavailable:  {
+      title: "Unavailable",
+      value: false,
+      action: [ actions.buttonAllOff ]
+    },
+    new:  {
+      title: "New",
+      value: false,
+      action: [ actions.buttonAllOff ]
+    }
+  };
 
 });
